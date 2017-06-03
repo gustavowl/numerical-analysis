0fb79be40501f50f1797f3106ed369092471becc
Gustavowl <gustavoalvesbezerra@gmail.com>
2017-06-03 13:08:09 +0000

    Implements opposite_signs bracketing method

---
+++ Unity2/opposite_signs.py
@@ -0,0 +1,47 @@
+import sys
+
+def function(f, x): #computes f(x), return it
+	y = 0
+	
+	for i in range(len(f)):
+		y += f[i] * x**i
+	return y
+
+
+def bracket_function(func, x, interval, max_iteractions):
+	#function is must be a list
+	
+	y1 = function(func, x)
+
+	for i in range(max_iteractions):
+		x += interval
+		y2 = function(func, x)
+		if y1 * y2 <= 0:
+			#found the closed interval that constais the root, return it
+			if (x - interval < x):
+				return [x - interval, x]
+			return [x, x - interval]
+		#otherwise compute next interval
+		y1 = y2
+	#else stop method
+	return None
+
+
+if len(sys.argv) < 5:
+	print("Invalid number of arguments. Read the instruction.md file")
+
+else:
+	filename = sys.argv[1] #file storing the function
+	x = sys.argv[2] #initial X
+	i = sys.argv[3] #pace
+	m = sys.argv[4] #number of iteractions
+
+	#generates function from file
+	with open(filename) as file:
+		content = file.read() #reads file. it should have only one line
+	file.close()
+	func = content.split(' ') #stores function
+	func = [float(x) for x in func] #converts elements from str to float
+	func = func[::-1] #inverts function so it can be properly processed
+
+	print(bracket_function(func, -10, 0.2, 50))
