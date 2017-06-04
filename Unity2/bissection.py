6ebf1e3c40bb17799a5398c00e223e7e553e28d6
Gustavo <gustavoalvesbezerra@gmail.com>
2017-06-04 13:07:36 -0300

    Updates comments on bissection method

--- Unity2/bissection.py
+++ Unity2/bissection.py
@@ -17,7 +17,7 @@ def bissection(func, interval_start, interval_end, iterations):
 	y2 = mod_op_signs.function(func, interval_end)
 
 	if iterations <= 0 or y1 >= 0 or y2 <= 0:
-		#returns whichever value is closes to the root
+		#returns whichever value is closest to the root
 		if math.fabs(y1) <= math.fabs(y2):
 			return interval_start
 		return interval_end
@@ -25,7 +25,7 @@ def bissection(func, interval_start, interval_end, iterations):
 	new_x = 0 #declaring auxiliary variables
 	y3 = 0 #declaring auxiliary variables
 
-	#executes bissection
+	#executes bissection method
 	for i in range(iterations):
 		new_x = (interval_start + interval_end) / 2
 		y3 = mod_op_signs.function(func, new_x)
