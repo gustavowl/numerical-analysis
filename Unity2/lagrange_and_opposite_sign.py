16daa1c27766d477b2533bb591579d218ebe0c44
Gustavo <gustavoalvesbezerra@gmail.com>
2017-06-04 16:06:37 -0300

    Removes mod_op_sign from lagrange's method

--- Unity2/lagrange_and_opposite_sign.py
+++ Unity2/lagrange_and_opposite_sign.py
@@ -1,12 +1,45 @@
-import mod_op_signs
 import math
 import sys
+
+def function(f, x): #computes f(x), return it
+	y = 0
+	
+	for i in range(len(f)):
+		y += f[i] * x**i
+	return y
+
+
+def bracket_function(func, x, interval, max_iterations):
+	#function is must be a list
+	y1 = function(func, x)
+
+	for i in range(max_iterations):
+		x += interval
+		y2 = function(func, x)
+		if y1 * y2 <= 0:
+			#found the closed interval that constais the root, return it
+			if (x - interval < x):
+				return [x - interval, x]
+			return [x, x - interval]
+		#otherwise compute next interval
+		y1 = y2
+	#else stop method
+	return None
+
+def get_function_from_file(filename):
+	#generates function from file
+	with open(filename) as file:
+		content = file.read() #reads file. it should have only one line
+	file.close()
+	func = content.split(' ') #stores function
+	func = [float(x) for x in func] #converts elements from str to float
+	return func[::-1] #inverts function so it can be properly processed
 
 #finds the limit of the function. Either inferior or superior.
 #it just applies L = 1 + (B/an)^(1/(n-k))
 def find_limit(func):
 	n = len(func) - 1
-	print("n = " + str(n))
+	#print("n = " + str(n))
 	if func[0] == 0 or func[n] <= 0:
 		#lagrange cannot be applied
 		return None
@@ -17,7 +50,7 @@ def find_limit(func):
 		if func[i] < 0:
 			k = i
 			break
-	print("k = " + str(k))
+	#print("k = " + str(k))
 
 	#finds b, the absolute value of the smallest coefficientes
 	b = func[i]
@@ -25,7 +58,7 @@ def find_limit(func):
 		if func[i] < b:
 			b = func[i]
 	b = math.fabs(b)
-	print("b = " + str(b))
+	#print("b = " + str(b))
 
 	return 1 + (b/func[n]) ** (1/(n-k))
 
@@ -42,7 +75,7 @@ def lagrange_bracketing(func):
 	#if an < 0, multiply function by -1
 	if func2[n] < 0:
 		func2 = [x*-1 for x in func2]
-		print(func2)
+		#print(func2)
 
 	inf = find_limit(func2)
 	return [inf, sup]
@@ -53,8 +86,8 @@ def lagrange_and_opposite_sign(func, delta):
 	max_iterations = math.ceil( (interval[1] - interval[0])
 		/ delta)
 
-	print([func, interval[0], delta, max_iterations])
-	interval = mod_op_signs.bracket_function(func, interval[0], 
+	#print([func, interval[0], delta, max_iterations])
+	interval = bracket_function(func, interval[0], 
 		delta, max_iterations)
 
 	print ("LaGrange after OppSign: " + str(interval))
@@ -69,5 +102,5 @@ if len(sys.argv) < 3:
 else:
 	filename = sys.argv[1] #file storing the function
 	delta = float(sys.argv[2]) #interval precision for opp_sign bracketing
-	func = mod_op_signs.get_function_from_file(filename)
-	print(lagrange_and_opposite_sign(func, delta))
+	func = get_function_from_file(filename)
+	lagrange_and_opposite_sign(func, delta)
