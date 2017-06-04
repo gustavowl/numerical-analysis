1037685e00fc93bacc4133050060f65014da0cee
Gustavo <gustavoalvesbezerra@gmail.com>
2017-06-04 15:29:50 -0300

    Updates bissection to work with eval()

--- Unity2/bissection.py
+++ Unity2/bissection.py
@@ -1,61 +1,5 @@
 import sys
-import mod_op_signs
-import math
-
-def bissection(func, interval_start, interval_end, iterations):
-	#verifies if input is valid
-	if (interval_start == interval_end):
-		return interval_start
-
-	if interval_start > interval_end:
-		#inverts interval if necessary
-		temp = interval_start
-		interval_start = interval_end
-		interval_end = temp
-
-	y1 = mod_op_signs.function(func, interval_start)
-	y2 = mod_op_signs.function(func, interval_end)
-
-	if iterations <= 0 or y1 >= 0 or y2 <= 0:
-		#returns whichever value is closest to the root
-		if math.fabs(y1) <= math.fabs(y2):
-			return interval_start
-		return interval_end
-
-	new_x = 0 #declaring auxiliary variables
-	y3 = 0 #declaring auxiliary variables
-
-	#executes bissection method
-	for i in range(iterations):
-		new_x = (interval_start + interval_end) / 2
-		y3 = mod_op_signs.function(func, new_x)
-
-		
-		#print(str(interval_start) + " " + str(new_x) + " " + str(interval_end))
-		#verifies if root
-		if y3 == 0: 
-			print("ITERATIONS: " + str(i + 1))
-			return new_x
-
-		#verifies if machine precision was reached
-		if new_x == interval_start:
-			print("ITERATIONS: " + str(i + 1))
-			return new_x
-		elif new_x == interval_end:
-			print("ITERATIONS: " + str(i + 1))
-			return new_x
-
-		#continues bissection
-		if y3 < 0:
-			interval_start = new_x
-			continue
-
-		interval_end = new_x
-
-	print("ITERATIONS: " + str(i + 1))
-	return new_x
-
-
+import mod_bissection
 
 if len(sys.argv) != 5:
 	print("Invalid number of arguments. Read the instruction.md file")
@@ -66,5 +10,7 @@ else:
 	interval_end = float(sys.argv[3])
 	iterations = int(sys.argv[4])
 
-	func = mod_op_signs.get_function_from_file(filename)
-	print(bissection(func, interval_start, interval_end, iterations))
+	with open(filename) as file:
+		func = file.read()
+	file.close()
+	print(mod_bissection.bissection(func, interval_start, interval_end, iterations))
